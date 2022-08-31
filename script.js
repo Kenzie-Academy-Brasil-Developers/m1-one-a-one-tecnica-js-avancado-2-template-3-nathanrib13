5bc8f35e99cb1f558110ca60b8205012bb4a38e9
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2022-08-31 13:43:53 -0300

    envio 1:1

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//Desenvolva seu código aqui!
+function verificaLista (list, num) {
+let count = 0 
+    for(let i = 0; i<list.length; i++){
+    if(list[i] === num){
+        count += 1  
+    }
+}
+
+if(count % 5 === 0 && count > 0) {
+    return "É multiplo de 5"
+}
+    return "Não é multiplo de 5"
+}
+
+
+const lista = [1, 1, 2, 1, 1,1,];
+console.log(verificaLista(lista, 1))    
